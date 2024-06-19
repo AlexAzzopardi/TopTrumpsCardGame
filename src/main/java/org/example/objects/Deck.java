fec63275ae1ef86a1494b2415d613cd5fee09bd6
Alex Azzopardi <alexazzopardi12@hotmail.com>
2024-06-19 22:12:14 +0100

    Random distribution of cards evenly into each deck

--- src/main/java/org/example/objects/Deck.java
+++ src/main/java/org/example/objects/Deck.java
@@ -2,7 +2,6 @@ package org.example.objects;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.util.List;
@@ -10,9 +9,14 @@ import java.util.List;
 @Getter
 @Setter
 @AllArgsConstructor
-@NoArgsConstructor
 public class Deck {
 
     private List<Card> deck;
+
+    public void ShowCards() {
+        for (Card card : this.deck) {
+            System.out.println(card.getName());
+        }
+    }
 
 }
